bebe999715f1d7b2cee08261c47cd0596254eba0
myusername <myemail>
2023-04-28 20:21:20 +0430

    button

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,28 +1,25 @@
 import "./App.css";
 import Website from "./components/welcome";
-import { ReactPropTypes } from "react";
+import counter from "./components/counter";
 const App = () => {
   const clickHandler = (name) => {
-    <div>
-      alert("Hi" + " " + name);
-    </div>;
+    <div>alert("Hi" + " " + name);</div>;
   };
   return (
     <div className="App">
+      {/*
+     
       <Website
         name="Mehran"
         age={41}
-        handler={() =>
-          clickHandler("Mehran")
-        }
+        handler={() => clickHandler("Mehran")}
       />
       <Website
         name="Mehrdad"
         age={32}
-        handler={() =>
-          clickHandler("Mehrdad")
-        }
-      />
+        handler={() => clickHandler("Mehrdad")}
+      />*/}
+      <Counter />
     </div>
   );
 };
--- react-basics/src/components/counter.jsx
+++ react-basics/src/components/counter.jsx
@@ -1,12 +1,12 @@
-const counter = () => {
-  return (
+const Counter = () => {
+  return [
     <div>
       <p>you clicked 2 times</p>
       <button onClick={() => alert("clicked")}>
         click me!
       </button>
-    </div>
-  );
+    </div>,
+  ];
 };
 
-export default counter;
+export default Counter;
