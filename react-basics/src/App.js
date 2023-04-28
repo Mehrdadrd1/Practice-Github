681a34c7bdcf0e40ec09d670ec2f6b032112455b
myusername <myemail>
2023-04-28 14:02:48 +0430

    First steps with react

---
+++ react-basics/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Welcome from "./components/welcome";
+const App = () => {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-basics/src/components/welcome.js
@@ -0,0 +1,5 @@
+const Welcome = () => {
+  return <h1>Welcome to React World Developer</h1>;
+};
+
+export default Welcome;
