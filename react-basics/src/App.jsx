3a68402902477035ef065adf223307db39c693ba
myusername <myemail>
2023-04-28 22:20:02 +0430

    handling events

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 import "./App.css";
 import Website from "./components/welcome";
 import Counter from "./components/counter/counter";
 const App = () => {
   const clickHandler = (name) => {
     <div>alert("Hi" + " " + name);</div>;
+  };
+  const actionLink = (e) => {
+    {
+      /*
+      use above func for preventing default behavior of func
+      */
+    }
+    e.preventDefault();
+    console.log("persiajs.dev");
+  };
+
+  const [isToggleOn, setIsToggleOn] =
+    useState(true);
+  const handletoggleclick = () => {
+    setIsToggleOn(!isToggleOn);
   };
   return (
     <div className="App">
@@ -19,7 +36,21 @@ const App = () => {
         age={32}
         handler={() => clickHandler("Mehrdad")}
       />*/}
+      {/*
       <Counter />
+      */}
+
+      <Counter />
+      <a
+        target="blank"
+        href="http://persiajs.dev"
+        onClick={actionLink}
+      >
+        Click ME
+      </a>
+      <button onClick={handletoggleclick}>
+        {isToggleOn ? `ON` : `OFF`}
+      </button>
     </div>
   );
 };
--- react-basics/src/components/counter/counter.jsx
+++ react-basics/src/components/counter/counter.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./counter.css";
-import styled from "styled-components";
-
+import Button from "./ButtonStyle";
 const Counter = () => {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
@@ -9,22 +8,6 @@ const Counter = () => {
     setName(e.target.value);
     console.log(name);
   };
-
-  const Button = styled.button`
-    cursor: pointer;
-    background: transparent;
-    font-size: 16px;
-    border-radius: 10%;
-    color: #ccc;
-    border: 2px solid #777;
-    margin: 0.5em;
-    padding: 0.25em;
-    transition: 0.5s all ease-out;
-    &:hover {
-      background-color: #ccc;
-      color: white;
-    }
-  `;
 
   return [
     <div>
@@ -38,6 +21,10 @@ const Counter = () => {
         name="name"
         value={name}
         onChange={handleChange}
+        onFocus={() =>
+          console.log("input is focused")
+        }
+        onBlur={() => console.log("input blured")}
       />
     </div>,
   ];
